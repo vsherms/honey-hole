4b059840d81939be615570c408244407b2f3263b
Neal Zucker <nealzucker@gmail.com>
2017-03-22 10:18:08 -0600

    background fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Home extends React.Component{
   render(){
     return (
-      <div className="home-background-container">
+      <div className="background-container2">
         <div className="page-container">
           <h2 className="title"> Home!!!</h2>
         </div>
